e12a2a8d607c775dd4114b6f157387ee1ed0aef3
Ram3ez <162774593+Ram3ez@users.noreply.github.com>
2024-10-06 15:00:49 +0530

    Update AboutUs.jsx

--- client/Game-Zee/src/Components/AboutUs/AboutUs.jsx
+++ client/Game-Zee/src/Components/AboutUs/AboutUs.jsx
@@ -13,9 +13,9 @@ const About = () => {
       <div><p className={styles.description}>Our team draws on expertise in astrophysics, game design, and education to create a game that is not just fun, but also scientifically accurate. From understanding star evolution to applying techniques like Radial Velocity and Transit Detection, each aspect of the game is designed to reflect authentic planetary science in a simplified and engaging format.</p></div>
       <div><p className={styles.description}>Meet the Creators</p></div>
       <div><p className={styles.description}>Behind Astron is a dedicated team of enthusiasts who believe in the power of games to teach and inspire:</p></div>
-      <div><p className={styles.description}>Space Enthusiasts: With a passion for the mysteries of the universe, we strive to make space science accessible to all.
-Educators: We ensure that every rule and concept in the game is backed by solid science, making learning a part of the fun.
-Game Designers: We work to balance educational content with engaging mechanics, ensuring the game remains strategic and enjoyable.
+      <div><p className={styles.description}>Space Enthusiasts: With a passion for the mysteries of the universe, we strive to make space science accessible to all.</p></div>
+      <div><p className={styles.description}>Educators: We ensure that every rule and concept in the game is backed by solid science, making learning a part of the fun.</p></div>
+      <div><p className={styles.description}>Game Designers: We work to balance educational content with engaging mechanics, ensuring the game remains strategic and enjoyable.
 Join Us on Our Mission!
 We believe that learning should never be limited to textbooks. With Astron, we aim to ignite a love for space and exploration in everyone, whether you’re a seasoned scientist or just someone who dreams of the stars.
 </p></div>
