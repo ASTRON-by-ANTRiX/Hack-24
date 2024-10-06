e7251267eb82a0d087f3d94426fbae38168a9784
ragulnathMB <ragulnath255@gmail.com>
2024-10-06 15:20:16 +0530

    updated home tagline

--- client/Game-Zee/src/Components/Home/Home.jsx
+++ client/Game-Zee/src/Components/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className={styles.container}>
       <div className={styles.title}>
         <div className={styles.mainTitle}><p>Astron</p></div>
-        <div className={styles.tagLine}><p>The Planet Hut</p></div>
+        <div className={styles.tagLine}><p>The Planet Hunt</p></div>
       </div>
       <div>
       <Link to="/game">
